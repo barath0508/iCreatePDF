d8e7956c54e63aca18f71565e5e1d65a5763dc89
Barath R <barath5727@gmail.com>
2026-07-21 21:27:14 +0530

    fix(adsense): update AdSense script strategy to afterInteractive for bot verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -177,7 +177,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8825674134696584"
           crossOrigin="anonymous"
-          strategy="lazyOnload"
+          strategy="afterInteractive"
         />
 
 
